2d9f57bbacfd3965a76af43cb16bb95062256f99
ssmahim01 <mahimzayn666@gmail.com>
2024-09-09 23:50:45 +0600

    Edited average.js file

--- average.js
+++ average.js
@@ -1,15 +1,6 @@
 // Return the average of values.
 
 function make_avg(arr, size){
-    if (arr.length !== size) {
-        throw new Error("The size of the array does not match the provided size.");
-    }
-    
-    if (size === 0) {
-        return 0; 
-        // Returning 0 or could handle it as an error depending on the context
-    }
-    
     let sum = arr.reduce((acc, num) => acc + num, 0);
     let average = sum / size;
 
